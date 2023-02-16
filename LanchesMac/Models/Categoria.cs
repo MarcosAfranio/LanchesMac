d28bb9b5f86134bdd6739ef3ecbac2bfcd16252c
MarcosAfranio <marcosafranio@outlook.com>
2023-02-16 10:54:48 -0300

    Projeto desable e Categoria CategoriaId

--- LanchesMac/Models/Categoria.cs
+++ LanchesMac/Models/Categoria.cs
@@ -2,11 +2,11 @@
 {
     public class Categoria
     {
-        public int CategoriaId { get; set; };
+        public int CategoriaId { get; set; }
         public string CategoriaNome { get; set; }
         public string Descricao { get; set; }
 
-        public List<Lanche> Lanches { get; set;}
+        public List<Lanche> Lanches { get; set; }
 
     }
 }
